024835dac3016654324255869cbc3750360d1525
youssef93u <youssef93oulaarabi@gmail.com>
2025-09-04 17:38:19 +0200

    Update mini projet.c4

--- mini projet.c
+++ mini projet.c
@@ -2,30 +2,52 @@
 
 int main() {
     int i;
-float t1,t2,t3,t4,t5,T,M;
-float notes[5]={t1,t2,t3,t4,t5},note_haute,note_basse;
-printf("entre les notes t1,t2,t3,t4,t5 :");
-scanf("%f %f %f %f %f",&t1,&t2,&t3,&t4,&t5);
-T=t1+t2+t3+t4+t5;
-printf("T=%.2f\n",T);
-M=T/5;
-printf("M=%.2f\n",M);
+float T,M;
+float notes[5];
+float note_haute,note_basse;
+for(i=0;i<5;i++){
+    printf(" notes[%d] :",i);
+    scanf("%f",&notes[i]);
+}
+T=0;
+for(i=0;i<5;i++){
+    T = T + notes[i];
+}
+printf(" total des notes %.2f:\n",T);
+M = T/5;
+ printf(" la moyenne %.2f:\n",M);
+ note_haute = notes[0];
+ for(i=0;i<5;i++){
+     if(note_haute<notes[i])
+  note_haute = notes[i];  
+}
+  printf("la note_haute %f :\n",note_haute);
+   note_basse = notes[0];
+ for(i=0;i<5;i++){
+     if(note_basse>notes[i])
+  note_basse = notes[i];  
+}
+  printf("la note_basse %f :\n",note_basse);
+if(T>=90) {
+    printf("excellent\n");
+}
+else if(T>=80 && T<=89) {
+    printf("tres bien\n");
+}
+else if(T>=70 && T<=79) {
+    printf("bien \n");
+}
+else if(T>=60 && T<=69) {
+    printf("passable \n");
+}
+else if(T<60) {
+    printf("Echec \n");
+}
 
- 
-if(M>=90) {
-    printf("excellent");
-}
-else if(M>=80 && M<=89) {
-    printf("tres bien");
-}
-else if(M>=70 && M<=79) {
-    printf("bien");
-}
-else if(M>=60 && M<=69) {
-    printf("passable");
-}
-else if(M<60) {
-    printf("Echec");
+if(M>=50){
+    printf("l étudiant a réussi \n");
+}else{
+    printf("l étudiant a échoué\n");
 }
 
 
